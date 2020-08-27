a442525c5bd2e0d73c96f2b8f2383fe7253359b9
edynak-demo <edynakdemo@gmail.com>
2020-08-27 07:17:41 -0400

    implemented index page,sign up, login forms

--- index.php
+++ index.php
@@ -1,3 +1,76 @@
 <?php 
   require 'core/init.php';
 ?>
+
+<html>
+	<head>
+		<title>KJconnect</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css"/>
+		<link rel="stylesheet" href="assets/css/style-complete.css"/>
+	</head>
+	<!--Helvetica Neue-->
+<body>
+<div class="front-img">
+	<img src="assets/images/background.jpg"></img>
+</div>	
+
+<div class="wrapper">
+<!-- header wrapper -->
+<div class="header-wrapper">
+	
+	<div class="nav-container">
+		<!-- Nav -->
+		<div class="nav">
+			
+			<div class="nav-left">
+				<ul>
+					<li><i class="fa fa-twitter" aria-hidden="true"></i><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+				</ul>
+			</div><!-- nav left ends-->
+
+			<div class="nav-right">
+				<ul>
+					<li><a href="#">Language</a></li>
+				</ul>
+			</div><!-- nav right ends-->
+
+		</div><!-- nav ends -->
+
+	</div><!-- nav container ends -->
+
+</div><!-- header wrapper end -->
+	
+<!---Inner wrapper-->
+<div class="inner-wrapper">
+	<!-- main container -->
+	<div class="main-container">
+		<!-- content left-->
+		<div class="content-left">
+			<h1>Welcome to KJconnec!</h1>
+			<br/>
+			<p>An uncensored place to connect with the KJ Scariest movie ever made community — and Get updates from the people you love, from the world and from things that interest you.</p>
+		</div><!-- content left ends -->	
+
+		<!-- content right ends -->
+		<div class="content-right">
+			<!-- Log In Section -->
+			<div class="login-wrapper">
+			  <?php require 'includes/login.php'; ?>
+			</div><!-- log in wrapper end -->
+
+			<!-- SignUp Section -->
+			<div class="signup-wrapper">
+			  <?php require 'includes/signup-form.php'; ?>
+			</div>
+			<!-- SIGN UP wrapper end -->
+
+		</div><!-- content right ends -->
+
+	</div><!-- main container end -->
+
+</div><!-- inner wrapper ends-->
+</div><!-- ends wrapper -->
+</body>
+</html>
