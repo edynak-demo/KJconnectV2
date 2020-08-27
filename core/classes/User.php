8a0bf7205c170ef44f00cf1f113334d9ba76c2ab
edynak-demo <edynakdemo@gmail.com>
2020-08-27 12:19:31 -0400

    implemented register method

--- core/classes/User.php
+++ core/classes/User.php
@@ -30,6 +30,17 @@ class User {
     }
   }
 
+  public function register($email, $screenName, $password){
+    $stmt = $this->pdo->prepare("INSERT INTO `users` (`email`, `password`, `screenName`, `profileImage`, `profileCover`) VALUES (:email, :password, :screenName, 'assets/images/profile/default.png','assets/images/background/defaultCoverImage.png' )");
+    $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+    $stmt->bindParam(":password", md5($password), PDO::PARAM_STR);
+    $stmt->bindParam(":screenName", $screenName, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $user_id = $this->pdo->lastInsertId();
+    $_SESSION['user_id'] = $user_id;
+  }
+
   public function userData($user_id){
     $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `user_id` = :user_id");
     $stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);
